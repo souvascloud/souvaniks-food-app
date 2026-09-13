48c78c811639a1d5aac2b208f7cab84d51f41039
Souvanik Saha <devsouva@gmail.com>
2026-09-13 11:55:16 +0530

    feat: enhance header with login toggle and profile section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,33 @@
 import "./Header.css";
 import logo from "../../assets/logo.png";
+import { useState } from "react";
+
 const Header = () => {
+  const [siginBtnName, setSignInButtonName] = useState("Login");
+
   return (
     <div className="header">
       <div className="logo">
         <img src={logo} alt="Souvanik's Food App Logo" />
       </div>
       <div className="location">📍 Kolkata , West Bengal,India</div>
-      <div className="cart">
+      <div className="header-actions">
         <button className="cart-button">Cart 🛒</button>
+        <div className="user-section">
+          {siginBtnName === "Logout" && (
+            <button className="profile-btn">👤</button>
+          )}
+          <button
+            className="login-btn"
+            onClick={() => {
+              setSignInButtonName((prev) =>
+                prev === "Login" ? "Logout" : "Login",
+              );
+            }}
+          >
+            {siginBtnName}
+          </button>
+        </div>
       </div>
     </div>
   );
